6178894cbafa7e20091a2396a5a0f696cc066e9a
AICO <16437571+ai-collaborative-operator@user.noreply.gitee.com>
2026-05-07 18:03:49 +0800

    '更新'

--- src/boot/element.ts
+++
@@ -1,9 +0,0 @@
-import { boot } from "quasar/wrappers";
-import ElementPlus  from 'element-plus';
-import 'element-plus/dist/index.css'
-import locale from 'element-plus/es/locale/lang/zh-cn';
-import 'dayjs/locale/zh-cn';
-export default boot(({ app }) => {
-  // Set ElementPlus instance on app
-  app.use(ElementPlus,{locale});
-});
--- src/pages/geographicInfo/foot/FootPage.vue
+++
@@ -1,9 +0,0 @@
-<!--底部界面-->
-<template>
-  <div class="foot-page"></div>
-</template>
-<script setup lang="ts" name="footPage">
-</script>
-<style scoped>
-
-</style>
